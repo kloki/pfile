b3a9c247c7ac7e2216724fbaa34b00636c8d9994
Koen Klinkers <k.klinkers@gmail.com>
2024-04-05 17:02:31 +0200

    add edit functionality

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,23 @@
-use std::{env, time::Duration};
+use std::{env, io, time::Duration};
 
 use indicatif::ProgressBar;
 use reqwest::{blocking::Client, header::HeaderMap};
 use serde::{Deserialize, Serialize};
 
-const INSTRUCTIONS: &str = "* Your are a helpfull assistent helping a developer.
+const GEN_INSTRUCTIONS: &str = "* Your are a helpfull assistent helping a developer.
 * You will be ask to generate new file or snippet in different programming languages.
 * You ONLY respond with the actual content of the file.
 * Don't wrap your response in markdown syntax.
 * Add explaination using comment blocks
 ";
 
+const EDIT_INSTRUCTIONS: &str = "* Your are a helpfull assistent helping a developer.
+* You will be ask to edit some code with some instructions.
+* To user will provide to be code in a markdown code block. The instructions after that.
+* You ONLY respond with the actual content of the file.
+* Don't wrap your response in markdown syntax.
+* Add explaination using comment blocks
+";
 #[derive(Serialize, Deserialize, Debug)]
 struct Message<'a> {
     role: &'a str,
@@ -24,13 +31,29 @@ struct PromptRequest<'a> {
 }
 
 impl PromptRequest<'_> {
-    fn new(prompt: &str) -> PromptRequest<'_> {
+    fn generate(prompt: &str) -> PromptRequest<'_> {
         PromptRequest {
             model: "gpt-3.5-turbo",
             messages: vec![
                 Message {
                     role: "system",
-                    content: INSTRUCTIONS,
+                    content: GEN_INSTRUCTIONS,
+                },
+                Message {
+                    role: "user",
+                    content: prompt,
+                },
+            ],
+        }
+    }
+
+    fn edit(prompt: &str) -> PromptRequest<'_> {
+        PromptRequest {
+            model: "gpt-3.5-turbo",
+            messages: vec![
+                Message {
+                    role: "system",
+                    content: EDIT_INSTRUCTIONS,
                 },
                 Message {
                     role: "user",
@@ -55,10 +78,13 @@ struct Payload {
     choices: Vec<Entry>,
 }
 
+fn read_from_stdin() -> String {
+    io::stdin().lines().map(|l| l.unwrap()).collect()
+}
+
 fn main() {
     let key = env::var("OPENAI_API_KEY").expect("OPENAI_API_KEY not set");
     let prompt: String = env::args().collect::<Vec<_>>()[1..].to_vec().join(" ");
-
     let mut headers = HeaderMap::new();
     headers.insert("Content-Type", "application/json".parse().unwrap());
     headers.insert(
@@ -70,10 +96,22 @@ fn main() {
     let bar = ProgressBar::new_spinner();
     bar.enable_steady_tick(Duration::from_millis(100));
 
-    let res = client
-        .post("https://api.openai.com/v1/chat/completions")
-        .json(&PromptRequest::new(&prompt))
-        .send();
+    let res = {
+        if prompt.starts_with("--") {
+            let snippet = read_from_stdin();
+            let prompt = format!("```{}``` {}", snippet, prompt);
+
+            client
+                .post("https://api.openai.com/v1/chat/completions")
+                .json(&PromptRequest::edit(&prompt))
+                .send()
+        } else {
+            client
+                .post("https://api.openai.com/v1/chat/completions")
+                .json(&PromptRequest::generate(&prompt))
+                .send()
+        }
+    };
     let payload: Payload = res.unwrap().json().expect("Failed response from openai");
 
     bar.finish();
